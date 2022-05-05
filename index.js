08cd6755e825f49f28d0ddec6ebec9517d5d4b86
Shahadat91 <shahadat20000@gmail.com>
2022-05-05 23:21:57 +0600

    add hero

--- index.js
+++ index.js
@@ -115,6 +115,10 @@ app.get('/', (req, res)=>{
     res.send('Running Genius Car Server');
 });
 
+app.get('/hero', (req, res) =>{
+  res.send('Hero Meets Hero Ku');
+})
+
 app.listen(port, ()=>{
     console.log('Listening to port', port);
 })
